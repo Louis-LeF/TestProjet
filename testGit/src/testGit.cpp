9fdf9bdd9104f4c1aefe1988eb38bc3cd33e1848
loulougm <louis.le.feuvre@gmail.com>
2017-03-17 11:52:12 +0100

    Tarek est con

---
+++ testGit/src/testGit.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : testGit.cpp
+// Author      : Louis
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Zozo t'es la plus belle !" << endl;
+	return 0;
+}
